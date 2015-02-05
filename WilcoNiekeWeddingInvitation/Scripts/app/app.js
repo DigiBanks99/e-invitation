d5f8a416a146fb2d833b076d4f1ecf9b6094285f
DigiBanks99 <wlboshoff@gmail.com>
2015-02-05 23:12:51 +0200

    Nice swing animation. Semi final

--- WilcoNiekeWeddingInvitation/Scripts/app/app.js
+++ WilcoNiekeWeddingInvitation/Scripts/app/app.js
@@ -1,292 +1,52 @@
-﻿'use strict';
+﻿var mapLoaded = false;
 
-var mapLoaded = false;
+setTransform = function(e, v) {
+    var s = e.style;
+    s.webkitTransform = 
+    s.MozTransform =
+    s.msTransform =
+    s.OTransform =
+    s.transform = v;
+};
 
-function PageInfo(element, success) {
-    this.emptyContentConst = "Nothing to show";
-    this.content, this.template, this.message;
+(function() {
+    var books = document.querySelectorAll('.book');
 
-    this.template = element.data().content;
-    if (!isUndefined(this.template)) {
-        // extend this as contentTypes increase
-        if (!isUndefined(this.template.picture)) {
-            this.content = '<img src="' + this.template.picture + '" />';
-        } else if (!isUndefined(this.template.message)) {
-            this.content = getMessage(this.template.message, function (value) {
-                success(value);
-                return value;
-            });
-        } else if (!isUndefined(this.template.map)) {
-            this.content = '<div>Map to Oak House</div>';
-        } else {
-            this.content = this.emptyContentConst;
+    for (var i = 0; i < books.length; i++) {
+        var book = books[i];
+        var pages = book.childNodes;
+        for (var j = 0; j < pages.length; j++) {
+            if (pages[j].tagName == "DIV") {
+                setTransform(pages[j], 'translate3d(0px, 0px, '+(-j)+'px)');
+            }
         }
+    }
+})();
+
+
+var px, py, sx, sy, down, rot = 0, rotX = 0, rotY = 0, cancel;
+var pages = document.querySelectorAll('#myBook > div > div');
+var currentPage = 0;
+
+var previousPage = function () {
+    if (currentPage > 0) {
+        currentPage--;
+        setTransform(pages[currentPage], 'translate3d(0px,0px,' + (-currentPage) + 'px) rotateY(0deg)');
+    }
+};
+var nextPage = function () {
+    if (currentPage < pages.length) {
+        setTransform(pages[currentPage], 'translate3d(0px,0px,' + currentPage + 'px) rotateY(-179deg)');
+        currentPage++;
     }
 };
 
-function initCover() {
-    //var $this = $("#toCover");
-    //if (typeof $this != 'undefined') {
-    //    var pageInfo = new PageInfo($this);
+var oakHouseLatlng = new google.maps.LatLng(-25.671085, 28.517962);
+var oakHouseMapOptions = { zoom: 16, center: oakHouseLatlng, mapTypeId: google.maps.MapTypeId.ROADMAP };
+var oakHouseMap = new google.maps.Map(document.getElementById("mapOakHouse"), oakHouseMapOptions);
+var oakHouseMarker = new google.maps.Marker({ position: oakHouseLatlng, map: oakHouseMap, title: "103 Oak Avenue" });
 
-    //    $("#space").removeClass('hide');
-
-    //    $("#coverCard").flip({
-    //        direction: "bt",
-    //        content: pageInfo.content,
-    //        onBefore: function () {
-    //            $(".revert").show();
-    //        }
-    //    });
-
-    //    getGuestHouses();
-
-    //    $("#toMap").hide();
-    //    $("#toMessage").hide();
-    //    $("#map").addClass('hide');
-    //    $("#toCover").hide();
-    //    $("#toInnerCover").show();
-    //    $("#pageCover").removeClass('hide');
-    //    $("#pageMessage").addClass('hide');
-    //    $("#pageGuestHouses").addClass('hide');
-    //}
-};
-
-function excuteJQuery() {
-    $("#coverPage").bind('click', function () {
-        var $this = $(this);
-
-        var hasFlip = false;
-        for (var i = 0; i < $this.context.classList.length; i++) {
-            if ($this.context.classList[i] == 'flip') {
-                hasFlip = true;
-                break;
-            }
-        }
-
-        if (hasFlip) {
-            $this.removeClass('flip');
-        } else {
-            $this.addClass('flip');
-        }
-    });
-
-    return;
-
-    ///////////////////////////////////////////
-    $("#toInnerCover").bind("click", function () {
-        var $this = $(this);
-        var pageInfo = new PageInfo($this);
-
-        $("#toMap").show();
-        $("#map").addClass('hide');
-        $("#toMessage").hide();
-        $("#toCover").show();
-        $("#toInnerCover").hide();
-        $("#pageCover").removeClass('hide');
-        $("#pageMessage").removeClass('hide');
-        $("#pageGuestHouses").addClass('hide');
-        $("#space").addClass('hide');
-
-        $("#coverCard").flip({
-            direction: "rl",
-            content: pageInfo.content,
-            onBefore: function () {
-                $(".revert").show();
-            }
-        });
-
-        var pageInfoContent = 'Not set';
-        var messagePageInfo = new PageInfo($("#toMessage"), function (response) {
-            pageInfoContent = response;
-
-            $("#messageCard").flip({
-                direction: "rl",
-                content: pageInfoContent
-            });
-        });
-        return false;
-    });
-
-    $("#toCover").bind("click", function () {
-        var $this = $(this);
-        var pageInfo = new PageInfo($this);
-
-        $("#space").removeClass('hide');
-
-        $("#coverCard").flip({
-            direction: "lr",
-            content: pageInfo.content,
-            onBefore: function () {
-                $(".revert").show();
-            }
-        });
-
-        $("#toMap").hide();
-        $("#map").addClass('hide');
-        $("#toCover").hide();
-        $("#toInnerCover").show();
-        $("#pageCover").removeClass('hide');
-        $("#pageMessage").addClass('hide');
-        $("#pageGuestHouses").addClass('hide');
-        return false;
-    });
-
-    $("#toMap").bind("click", function () {
-        var $this = $(this);
-        var mapPageInfo = new PageInfo($this);
-
-        $("#map").addClass('hide');
-        $("#toMap").hide();
-        $("#toInnerCover").hide();
-        $("#toCover").hide();
-        $("#toMessage").show();
-        $("#pageCover").addClass('hide');
-        $("#pageMessage").removeClass('hide');
-        $("#pageGuestHouses").removeClass('hide');
-
-        $("#messageCard").flip({
-            direction: "lr",
-            content: mapPageInfo.content,
-            onBefore: function () {
-                $(".revert").show();
-            },
-            onEnd: function (element) {
-                loadMapScript(function () {
-                    $("#map").removeClass('hide');
-                })
-            }
-        });
-        return false;
-    });
-
-    $("#toMessage").bind("click", function () {
-        var $this = $(this);
-
-        var pageInfoContent = 'Not set';
-        var messagePageInfo = new PageInfo($this, function (response) {
-            pageInfoContent = response;
-
-            $("#messageCard").flip({
-                direction: "rl",
-                content: pageInfoContent
-            });
-        });
-
-        $("#toMap").show();
-        $("#map").addClass('hide');
-        $("#toInnerCover").hide();
-        $("#toCover").show();
-        $("#toMessage").hide();
-        $("#pageCover").removeClass('hide');
-        $("#pageMessage").removeClass('hide');
-        $("#pageGuestHouses").addClass('hide');
-        return false;
-    })
-};
-
-function initializeMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(-25.670848, 28.517866),
-        zoom: 17,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    var element = document.getElementById("map");
-    if (!isNullOrEmpty(element))
-        var map = new google.maps.Map(element, mapProp);
-};
-
-function getMessage(key, callBack, callBackObj) {
-    var message = '';
-    switch (key) {
-        case "welcomeMessage":
-            $.getJSON("http://localhost:1468/Content/json/message.json", function (data) {
-                var boodskap = data.message;
-                message = '<div class="container-message">' +
-                            '<div class="welcome-message">' +
-                                 '<p class="small"><span class="message-title">' + boodskap.title + '</span></p>' +
-                                 '<p class="small"><span>' + boodskap.main + '</span></p>' +
-                                 '<p class="small"><span>' + boodskap.sub + '</span></p>' +
-                                 '<p class="small"><span>' + boodskap.postsub + '</span></p>' +
-                                 '<p class="small"><span>' + boodskap.rsvp + '</span><br />';
-                for (var i = 0; i < boodskap.contacts.length; i++) {
-                    message += '<span>' + boodskap.contacts[i].person + '</span><br />';
-                }
-                message +=       '</p>' +
-                                 '<p class="small"><span>' + boodskap.gift + '</span>' +
-                                 '<i class="fa fa-envelope-o" aria-hidden="true"></i></p>' + // TODO: add envelop
-                                 '<p class="small"><span>' + boodskap.footnote + '</span></p>' +
-                             '</div></div>';
-
-                callBack(message);
-                return message;
-            });
-            break;
-        default:
-            message = 'Nothing to show';
-            break;
-    }
-    return message;
-};
-
-function getGuestHouses() {
-    var $this = $("#guestHousesCard");
-    $.getJSON("http://localhost:1468/Content/json/guesthouses.json", function (data) {
-        if (isNullOrEmpty(data))
-            return;
-
-        var content = '<div class="brown-shadow">' +
-            '<ul class="list-group">' +
-            '<li class="list-group-item guesthouse guesthouse-description">Verblyf/Akkomodasie:<br />------------------------------</li>';
-        for (var i = 0; i < data.length; i++) {
-            var guesthouse = data[i];
-            content += '<li class="list-group-item guesthouse">';
-            content += '<p class="small">';
-            content += '<span>' + guesthouse.name + '</span><br />';
-            content += '<span>' + guesthouse.address + '</span><br />';
-            content += '<span class="guesthouse-tel">' + guesthouse.tel + '</span><br />';
-            if (guesthouse.coordinates != null) {
-                content += '<span class="guesthouse-coordinates">' + guesthouse.coordinates + '</span><br />';
-            }
-            content += '</p>';
-            content += '</li>';
-        }
-        content += '</ul></div>';
-        $this.append(content);
-        $this.html()
-    })
-    return;
-};
-
-function loadMapScript(callback) {
-    if (mapLoaded) {
-        callback();
-        return;
-    }
-
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
-        '&signed_in=true&callback=initializeMap';
-    document.body.appendChild(script);
-    mapLoaded = true;
-
-    if (typeof callback === 'function') {
-        callback(script);
-    }
-
-    return { then: callback };
-};
-
-function isUndefined(value) {
-    if (typeof value === 'undefined')
-        return true;
-    return false;
-};
-
-function isNullOrEmpty(value) {
-    if (isUndefined(value) || value === null || (!isUndefined(value) && value.length == 0))
-        return true;
-    return false;
-};
+var chapelLatlng = new google.maps.LatLng(-25.673090, 28.520988);
+var chapelMapOptions = { zoom: 16, center: chapelLatlng, mapTypeId: google.maps.MapTypeId.ROADMAP };
+var chapelMap = new google.maps.Map(document.getElementById("mapChapel"), chapelMapOptions);
+var chapelMarker = new google.maps.Marker({ position: chapelLatlng, map: chapelMap, title: "Sand Stone Chapel" });
